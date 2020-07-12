5d79a24b27eaf4acb5dad209ee841be93030776f
fatimanawmi <fatimanawmi@gmail.com>
2020-07-12 16:20:22 +0600

    dml work'
    ;

--- Zarad/core_website/views.py
+++ Zarad/core_website/views.py
@@ -6,6 +6,31 @@ from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import reverse
 from django.contrib.auth.decorators import login_required
 
+def accountType(email):
+    cursor.execute("SELECT SELLER_ID FROM SELLER WHERE EMAIL_ID = email")
+    results = cursor.fetchall()
+    if(len(results) == 0):
+        cursor.execute("SELECT EMPLOYEE_ID FROM DELIVERY_GUY WHERE EMAIL_ID = email")
+        results = cursor.fetchall()
+        if(len(results) == 0):
+            cursor.execute("SELECT CUSTOMER_ID FROM CUSTOMER WHERE EMAIL_ID = email")
+            results = cursor.fetchall()
+            if(len(results) == 0):
+                  cursor.execute("SELECT EMPLOYEE_ID FROM CUSTOMER_CARE_EMPLOYEE WHERE EMAIL_ID = email")
+                  results = cursor.fetchall()
+                  if(len(results) == 0):
+                      cursor.execute("SELECT EMPLOYEE_ID FROM ADMIN WHERE EMAIL_ID = email")
+                      results = cursor.fetchall()
+                      if(len(results) != 0):
+                          return 'admin'
+                  else :
+                      return 'customerCare'
+            else:
+                return 'customer'
+        else:
+            return 'deliveryGuy'
+    else:
+        return 'seller'
 # Create your views here.
 def home_page(request):
     isloggedin = False
@@ -15,6 +40,5 @@ def home_page(request):
         if request.session['useremail'] == 'nazmultakbir98@gmail.com' or request.session['useremail'] == 'fatimanawmi@gmail.com':
             accountType = 'admin'
         else:
-            'check account type from database'
-            accountType = 'customer'
+            accountType = accountType(request.session['useremail'])
     return render(request, "home_page.html", {'isloggedin': isloggedin, 'accountType': accountType})
