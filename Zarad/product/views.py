bc3c369c55a6910112052661a37b231b73461f30
Nazmul Takbir <nazmultakbir98@gmail.com>
2020-07-12 21:49:38 +0600

    search page design

--- Zarad/product/views.py
+++ Zarad/product/views.py
@@ -37,4 +37,41 @@ def search_result(request, search_string):
             " check accountType from database using request.session['useremail'] "
             accountType = 'customer'
 
+    if search_string.startswith('category_') and len(search_string)>len('category_'):
+        category = search_string[len('category_'):]
+        temp = ''
+        for i in category.split('_'):
+            if len(i)>0:
+                temp += i + ' '
+        category = temp[:-1]
+        category = category.replace('and', '&')
+        category = category.replace('Mens Fashion', 'Men\'s Fashion')
+        category = category.replace('Womens Fashion', 'Women\'s Fashion')
+
+        if ' check if category is one of the categories from the category table ':
+            ' check the product table and extract all products with the given category '
+    elif search_string == 'Offers_Only':
+        ' check the offer table and extract all offers that have not expired yet '
+
+    words = []
+    for i in search_string.split('_'):
+        if len(i)>0:
+            words.append(i)
+    search_string = ' '.join(words)
+
+    """
+        create the following view for each product item:
+            product id, product name, category_name, description, seller id, seller name
+        ' check if search_string completely matches product_id '
+        ' check if search_string completely matches product_name '
+        ' check if search_string completely matches a category name '
+        ' check if search_string completely mathces a seller name '
+        ' check for products whose name has EDS > 50 when compared to search_string. select the top 15 '
+        ' check for products whose name has EDS > 50 when compared to first quarter of search_string. select the top 15 '
+        ' check for products whose name has EDS > 50 when compared to second quarter of search_string. select the top 5 '
+        ' check for products whose name has EDS > 50 when compared to third quarter of search_string. select the top 5 '
+        ' check for products whose name has EDS > 50 when compared to fourth quarter of search_string. select the top 5 '
+    """
+    "features"
+
     return render(request, 'search_result.html', {'isloggedin': isloggedin, 'accountType': accountType})
